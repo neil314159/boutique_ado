c2b52d5b7b71eb5dde0578d2545c347dfa846d87
neil314159 <neil@neilboland.com>
2022-07-15 01:02:18 +0000

    Add admin sorting and categories

--- products/models.py
+++ products/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 
 class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+        
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
@@ -9,7 +13,6 @@ class Category(models.Model):
 
     def get_friendly_name(self):
         return self.friendly_name
-
 
 class Product(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
